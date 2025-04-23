4f5acc9c7f2e04ecf6cfe4db5f3d5f866fca6900
Symul Kabir <symulkabirpranta@gmail.com>
2025-04-24 00:39:37 +0600

    Complted scraping and action

--- src/js/popup.js
+++ src/js/popup.js
@@ -8,9 +8,37 @@ document.getElementById("clickSubmit").addEventListener("click", () => {
 });
 // text-token-text-error
 const clickSubmitButton = async () => {
-  const serverDomain = "https://server.somacharnews.com"; 
-  const processProgress = () => {
+  const serverDomain = "https://server.somacharnews.com";
+  console.log("hello wrold");
+  const scrapeHandler = () => {
+    const searchInputField = document.querySelector("input.Ax4B8.ZAGvjd");
+    if (searchInputField) {
+      console.log("hello if");
+      searchInputField.value =
+        "https://somacharnews.com/news/680915df27070bc4e7aca4b7";
+      searchInputField.dispatchEvent(new Event("input", { bubbles: true }));
+      searchInputField.dispatchEvent(new Event("change", { bubbles: true }));
+    }
+    const searchButton = document.querySelector('button[aria-label="Search"]');
 
+    if (searchButton) {
+      searchButton.click();
+    }
+    console.log("hello form before time");
+
+    setTimeout(() => {
+      console.log("time complete");
+    }, 2000);
+    console.log("hello form after time");
+    const indexRequestButton = document.querySelector("span.cTsG4");
+    //  const indexRequestButton = document.querySelector('span.cTsG4');
+    if (indexRequestButton) {
+      indexRequestButton.click();
+    }
+  };
+
+  const processProgress = () => {
+    scrapeHandler();
   };
 
   try {
